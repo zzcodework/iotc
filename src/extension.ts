4231caffd1bf4242dc4bcaa57f6c56147486ac2d
zz <zhengzh@microsoft.com>
2021-02-19 17:55:00 -0800

    2nd command

--- src/extension.ts
+++ src/extension.ts
@@ -19,14 +19,13 @@ export function activate(context: vscode.ExtensionContext) {
 			// The code you place here will be executed every time your command is executed
 			// Display a message box to the user
 			vscode.window.showInformationMessage('Hello World from iotc!');
-		})
-	);
+		}
+	));
 
 	context.subscriptions.push(vscode.commands.registerCommand(
 		'iotc.test',
 		async () => {
 			const result = await vscode.window.showInformationMessage('Test', 'Yes', 'No');
-
 			if (result === 'Yes') {
 				vscode.window.showInformationMessage('It is yes');
 			}
