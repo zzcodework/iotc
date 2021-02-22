dc7bc4ae5227f41fafbcc74d8735afb0a1c4a1dd
zz <zhengzh@microsoft.com>
2021-02-22 13:53:55 -0800

    Fix webpack

--- src/service/central.ts
+++ src/service/central.ts
@@ -5,16 +5,18 @@ const codeId = getMachineName();
 
 export async function connectToCentral(): Promise<void> {
     try {
-        console.log(codeId);
+        console.log(`VSCode id: ${codeId}`);
 
         const tokens = await listTokens();
-        console.log(tokens);
+        console.log(`List tokens: ${tokens.length}`);
 
         let token = await getToken(codeId);
+        console.log(`Get existing token: ${token}`);
+
         if (!token) {
             token = await createToken(codeId);
+            console.log(`Create new token: ${token}`);
         }
-
     }
     catch (e) {
         console.error(e);
